7073b7611f1ff5f93aa921a2830800bfa19f3d65
Tim Bunce <Tim.Bunce@pobox.com>
2019-07-11 15:46:12 +0100

    Export ErrUnsupportedPlatform error
    Signed-off-by: Tim Bunce <Tim.Bunce@pobox.com>

--- keyring_fallback.go
+++ keyring_fallback.go
@@ -5,19 +5,19 @@ import (
 	"runtime"
 )
 
-// All of the following method errors out on unsupported platforms
-var errUnsupportedPlatform = errors.New("Unsupported platform: " + runtime.GOOS)
+// All of the following methods error out on unsupported platforms
+var ErrUnsupportedPlatform = errors.New("Unsupported platform: " + runtime.GOOS)
 
 type fallbackServiceProvider struct{}
 
 func (fallbackServiceProvider) Set(service, user, pass string) error {
-	return errUnsupportedPlatform
+	return ErrUnsupportedPlatform
 }
 
 func (fallbackServiceProvider) Get(service, user string) (string, error) {
-	return "", errUnsupportedPlatform
+	return "", ErrUnsupportedPlatform
 }
 
 func (fallbackServiceProvider) Delete(service, user string) error {
-	return errUnsupportedPlatform
+	return ErrUnsupportedPlatform
 }
